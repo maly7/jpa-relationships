49cd70fc004a2abf950564c1ab8c2307bbad1339
Paul Aly <paul.aly@outlook.com>
2017-07-30 18:57:10 -0400

    removing comments

--- src/test/groovy/com/github/maly7/domain/NonTransactionalBookCreationSpec.groovy
+++ src/test/groovy/com/github/maly7/domain/NonTransactionalBookCreationSpec.groovy
@@ -22,15 +22,7 @@ class NonTransactionalBookCreationSpec extends IntegrationSpec {
         Book bookWithAuthor = bookRepository.save(book)
 
         then: 'The book with the author has been persisted'
-        bookWithAuthor.authors.every { it.id && it.name } // This fails because the author name info isn't saved
-        /*
-        Generated sql for this:
-        Hibernate: insert into book (id, title) values (null, ?)
-        Hibernate: select book0_.id as id1_0_0_, book0_.title as title2_0_0_ from book book0_ where book0_.id=?
-        Hibernate: select authors0_.book_id as book_id2_1_0_, authors0_.author_id as author_i1_1_0_, authors0_.author_id as id1_2_1_, authors0_1_.name as name2_2_1_, authors0_.book_id as book_id2_1_1_ from book_author authors0_ inner join writer authors0_1_ on authors0_.author_id=authors0_1_.id where authors0_.book_id=?
-        Hibernate: insert into writer (id, name) values (null, ?)
-        Hibernate: insert into book_author (book_id, author_id) values (?, ?)
-        */
+        bookWithAuthor.authors.every { it.id && it.name }
     }
 
     void 'Not setting the book on the BookAuthor should persist the BookAuthor'() {
@@ -43,14 +35,6 @@ class NonTransactionalBookCreationSpec extends IntegrationSpec {
 
         then: 'The book with teh author should be persisted'
         bookWithAuthor.authors.every { it.id && it.name }
-        /*
-        This generates the same sql as the previous test
-        Hibernate: insert into book (id, title) values (null, ?)
-        Hibernate: select book0_.id as id1_0_0_, book0_.title as title2_0_0_ from book book0_ where book0_.id=?
-        Hibernate: select authors0_.book_id as book_id2_1_0_, authors0_.author_id as author_i1_1_0_, authors0_.author_id as id1_2_1_, authors0_1_.name as name2_2_1_, authors0_.book_id as book_id2_1_1_ from book_author authors0_ inner join writer authors0_1_ on authors0_.author_id=authors0_1_.id where authors0_.book_id=?
-        Hibernate: insert into writer (id, name) values (null, ?)
-        Hibernate: insert into book_author (book_id, author_id) values (?, ?)
-         */
     }
 
     void 'A book with an author should be persisted at the same time'() {
